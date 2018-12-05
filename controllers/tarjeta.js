1fbdbf63934ee904f5d22246abbc8025ab28fb53
Carlos Hernández <CarlosHdz_97@outlook.com>
2018-12-04 20:01:40 -0600

    mas cambios

--- controllers/tarjeta.js
+++ controllers/tarjeta.js
@@ -7,25 +7,11 @@ module.exports = {
       mm:req.body.mm,
       aaaa:req.body.aaaa,
       cvc:req.body.cvc,
-      nombre_tarjeta:req.body.nombre_tarjeta,
-      userId: req.session.userId
+      nombre_tarjeta:req.body.nombre_tarjeta
+      userId: session.userId
     };
     Cards.create(data).then(result=>{
-      if(result){
-        res.render('card/show',{message: {
-          title:"Registro exitoso",
-          txt:"la tarjeta se registro",
-          icon:"success",
-          button:"Aceptar"
-        }});
-      }else{
-        res.render('card/show',{message: {
-          title:"Usuario o contraseña incorrecta",
-          txt:"Intentelo nuevamente",
-          icon:"error",
-          button:"Aceptar"
-        }});
-      }
+      res.redirect('/formaPago');
       }).catch(err=>{
         res.json(err);
     });
